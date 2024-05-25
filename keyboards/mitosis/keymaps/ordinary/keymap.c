b880a1fbe8dca81c884385726b9cf98b334682ef
盒子 <i@box.gd>
2024-05-25 21:28:17 +0800

    MO -> TT

--- keyboards/mitosis/keymaps/ordinary/keymap.c
+++ keyboards/mitosis/keymaps/ordinary/keymap.c
@@ -10,7 +10,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+--------+--------+--------+--------| |--------+--------+--------+--------+--------|
  * | Z      | X      | C      | V      | B      | | N      | M      | ,<     | .>     | /?     |
  * '--------+--------+--------+--------+--------| |--------+--------+--------+--------+--------'
- *          | ESC    | ⇧ L    | MO(2)  | MO(1)  | | F19    | TG(3)  | ⇧ R    | ⌫      |
+ *          | ESC    | ⇧ L    | TT(2)  | TT(1)  | | F19    | TG(3)  | ⇧ R    | ⌫      |
  *          |--------+--------+--------+--------| |--------+--------+--------+--------|
  *          | TAB    | ⌥ L    | ⌘ L    | L Ctrl | | Space  | ⇧ R    | R Ctrl | ↵      |
  *          '-----------------------------------' '-----------------------------------'
@@ -20,7 +20,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_Q,    KC_W,    KC_E,     KC_R,     KC_T,      KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,
   KC_A,    KC_S,    KC_D,     KC_F,     KC_G,      KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,
   KC_Z,    KC_X,    KC_C,     KC_V,     KC_B,      KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,
-           KC_ESC,  KC_LSFT,  MO(2),    MO(1),     KC_F19,   TT(3),    KC_RSFT,  KC_BSPC,
+           KC_ESC,  KC_LSFT,  TT(2),    TT(1),     KC_F19,   TT(3),    KC_RSFT,  KC_BSPC,
            KC_TAB,  KC_LALT,  KC_LGUI,  KC_LCTL,   KC_SPC,   KC_RSFT,  KC_RCTL,    KC_ENT
 ),
 
