acea3eeff3e7b6744e4cc13fc6c2c97f74ba27a7
盒子 <i@box.gd>
2024-05-25 22:33:16 +0800

    小键盘层加减乘除

--- keyboards/mitosis/keymaps/ordinary/keymap.c
+++ keyboards/mitosis/keymaps/ordinary/keymap.c
@@ -53,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+--------+--------+--------+--------| |--------+--------+--------+--------+--------|
  * | F18    | F9     | F10    | F11    | F12    | | F13    | F14    | F15    | F16    | F17    |
  * '--------+--------+--------+--------+--------| |--------+--------+--------+--------+--------'
- *          | xxxxxx | xxxxxx | -voov- | xxxxxx | | xxxxxx | xxxxxx | ⇧ R    | ⌫      |
+ *          | xxxxxx | xxxxxx | -voov- | xxxxxx | | Space  | xxxxxx | ⇧ R    | ⌫      |
  *          |--------+--------+--------+--------| |--------+--------+--------+--------|
  *          | xxxxxx | xxxxxx | xxxxxx | xxxxxx | | R Ctrl | ⌘ R    | ⌥ R    | ↵      |
  *          '-----------------------------------' '-----------------------------------'
@@ -62,29 +62,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC, KC_F1,    KC_F2,    KC_F3,    KC_F4,     KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_BSPC,
   KC_TAB, KC_F5,    KC_F6,    KC_F7,    KC_F8,     KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  KC_ENT,
   KC_F18, KC_F9,    KC_F10,   KC_F11,   KC_F12,    KC_F13,   KC_F14,   KC_F15,   KC_F16,   KC_F17,
-          KC_NO,    KC_NO,    KC_TRNS,  KC_NO,     KC_NO,    KC_NO,    KC_RSFT,  KC_BSPC,
+          KC_NO,    KC_NO,    KC_TRNS,  KC_NO,     KC_SPC,   KC_NO,    KC_RSFT,  KC_BSPC,
           KC_NO,    KC_NO,    KC_NO,    KC_NO,     KC_RCTL,  KC_RGUI,  KC_RALT,  KC_ENT
 ),
 
 /*
  * .--------------------------------------------. .--------------------------------------------.
- * | xxxxxx | xxxxxx | Insert | Home ↖ | Page ⇞ | | 7      | 8      | 9      | xxxxxx | xxxxxx |
+ * | xxxxxx | xxxxxx | Insert | Home ↖ | Page ⇞ | | 7      | 8      | 9      | ÷      | -      |
  * |--------+--------+--------+--------+--------| |--------+--------+--------+--------+--------|
- * | xxxxxx | xxxxxx | Delete | End  ↘ | Page ⇟ | | 4      | 5      | 6      | xxxxxx | xxxxxx |
+ * | xxxxxx | xxxxxx | Delete | End  ↘ | Page ⇟ | | 4      | 5      | 6      | x      | +      |
  * |--------+--------+--------+--------+--------| |--------+--------+--------+--------+--------|
  * | xxxxxx | xxxxxx | xxxxxx | xxxxxx | xxxxxx | | 1      | 2      | 3      | xxxxxx | ↵      |
  * '--------+--------+--------+--------+--------| |--------+--------+--------+--------+--------'
- *          | xxxxxx | xxxxxx | xxxxxx | xxxxxx | | xxxxxx | -voov- | xxxxxx | ⌫      |
+ *          | xxxxxx | xxxxxx | xxxxxx | xxxxxx | | Space  | -voov- | C/AC   | ⌫      |
  *          |--------+--------+--------+--------| |--------+--------+--------+--------|
- *          | xxxxxx | xxxxxx | xxxxxx | xxxxxx | | xxxxxx | 0      | .      | ↵      |
+ *          | xxxxxx | xxxxxx | xxxxxx | xxxxxx | | 0      | 0      | .      | ↵      |
  *          '-----------------------------------' '-----------------------------------'
  */
 [3] = LAYOUT(
-  KC_NO,  KC_NO,    KC_INS,   KC_HOME,  KC_PGUP,   KC_P7,    KC_P8,    KC_P9,    KC_NO,     KC_NO,
-  KC_NO,  KC_NO,    KC_DEL,   KC_END,   KC_PGDN,   KC_P4,    KC_P5,    KC_P6,    KC_NO,     KC_NO,
+  KC_NO,  KC_NO,    KC_INS,   KC_HOME,  KC_PGUP,   KC_P7,    KC_P8,    KC_P9,    KC_PSLS,   KC_PMNS,
+  KC_NO,  KC_NO,    KC_DEL,   KC_END,   KC_PGDN,   KC_P4,    KC_P5,    KC_P6,    KC_PAST,   KC_PPLS,
   KC_NO,  KC_NO,    KC_NO,    KC_NO,    KC_NO,     KC_P1,    KC_P2,    KC_P3,    KC_NO,     KC_PENT,
-          KC_NO,    KC_NO,    KC_NO,    KC_NO,     KC_NO,    KC_TRNS,  KC_NO,    KC_BSPC,
-          KC_NO,    KC_NO,    KC_NO,    KC_NO,     KC_NO,    KC_P0,    KC_PDOT,  KC_ENT
+          KC_NO,    KC_NO,    KC_NO,    KC_NO,     KC_SPC,   KC_TRNS,  KC_ESC,   KC_BSPC,
+          KC_NO,    KC_NO,    KC_NO,    KC_NO,     KC_P0,    KC_P0,    KC_PDOT,  KC_ENT
 )
 
 };
